80d835492bca155f5d4e740541d5e0d9774720e6
michael-j-potts <68249847+michael-j-potts@users.noreply.github.com>
2024-02-12 19:07:08 -0700

    Update Mimic3.py

--- Mimic3.py
+++ Mimic3.py
@@ -12,20 +12,24 @@ def death_age(rows):
     
 def threedigiticd(codes):
     if isinstance(codes, list):
-        print(codes)
-        return [code for code in codes if code.isdigit()][:3]
+        icd9_list = []
+        for code in codes:
+            if code != 'nan':
+                icd9_list = ["".join(filter(str.isdigit, str(code)))[:3] for code in codes]
+        return icd9_list
+                    
     else:
         return []
     
     
-Admissions = pd.read_csv("C:\\Users\\micha\\Documents\\Data science projects\\Mimic_datasets\\ADMISSIONS.csv")
-Diagnoses = pd.read_csv("C:\\Users\\micha\\Documents\\Data science projects\\Mimic_datasets\\DIAGNOSES_ICD.csv")
-Labs = pd.read_csv("C:\\Users\\micha\\Documents\\Data science projects\\Mimic_datasets\\LABEVENTS.csv")
-Patients = pd.read_csv("C:\\Users\\micha\\Documents\\Data science projects\\Mimic_datasets\\PATIENTS.csv")
-#Prescriptions = pd.read_csv("C:\\Users\\micha\\Documents\\Data science projects\\Mimic_datasets\\PRESCRIPTIONS.csv")
+Admissions = pd.read_csv("C:\\Users\\micha\\Documents\\Data science projects\\Mimic_project\\Mimic_datasets\\ADMISSIONS.csv")
+Diagnoses = pd.read_csv("C:\\Users\\micha\\Documents\\Data science projects\\Mimic_project\\Mimic_datasets\\DIAGNOSES_ICD.csv")
+Labs = pd.read_csv("C:\\Users\\micha\\Documents\\Data science projects\\Mimic_project\\Mimic_datasets\\LABEVENTS.csv")
+Patients = pd.read_csv("C:\\Users\\micha\\Documents\\Data science projects\\Mimic_project\\Mimic_datasets\\PATIENTS.csv")
+#Prescriptions = pd.read_csv("C:\\Users\\micha\\Documents\\Data science projects\\Mimic_project\\Mimic_datasets\\PRESCRIPTIONS.csv")
 
 Dataset = pd.DataFrame()
-Dataset['PatientID'] = Patients['SUBJECT_ID']
+Dataset['SUBJECT_ID'] = Patients['SUBJECT_ID']
 Dataset['Alive'] = Patients['EXPIRE_FLAG'] == 0
 Dataset["Death_Age"] = Patients[['DOB', 'DOD']].apply(death_age, axis = 1)
 
@@ -38,6 +42,7 @@ Labs = Labs[mask].reset_index(drop=True)
 Labs = Labs.iloc[:,[1,2,3,8]]
 
 Patient_Data = pd.merge(Admissions, Diagnoses, on=['SUBJECT_ID', "HADM_ID"], how = 'left')
+Patient_Data = pd.merge(Dataset, Patient_Data, on=['SUBJECT_ID'], how = 'left')
 Patient_Data['ICD9_CODE'] = Patient_Data['ICD9_CODE'].apply(threedigiticd)
 
 print(Patient_Data.head(100))
